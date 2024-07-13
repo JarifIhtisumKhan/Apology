d8ad47d8cfffad8de28e7b49ba076d9cf7f3954d
Imtiazkun <alshahriar.me@gmail.com>
2024-07-13 17:20:15 +0600

    Pushed gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/JarifIhtisumKhan/", // Replace with your repository name
+  base: "/Apology/", // Replace with your repository name
   plugins: [react()],
 });
